52cba1ed998db815fe598d6f45d62710e9eb1569
Artem <arlomov@gmail.com>
2021-05-28 19:31:32 +0300

    create: slider reviews  and fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
-const swiper = new Swiper('.swiper-container', {
+const hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
   
     // Navigation arrows
     navigation: {
-      nextEl: '.slider-button_next',
-      prevEl: '.slider-button_prev',
+      nextEl: '.hotel-slider__button_next',
+      prevEl: '.hotel-slider__button_prev',
     },
     keyboard: {
         enabled: true,
         onlyInViewport: false,
-      },
+    },
   });
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button_next',
+    prevEl: '.reviews-slider__button_prev',
+  },
+  keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+  },
+});
