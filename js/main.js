1d66c245d48dfa0ffe2e4cd7e7320edd00428bf7
Artem <arlomov@gmail.com>
2021-06-16 16:54:29 +0300

    create: jquery modal window & fix menu jquery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+  const hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
     loop: true,
   
@@ -28,8 +29,28 @@ const reviewsSlider = new Swiper('.reviews-slider', {
   },
 });
 
-var menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function () {
-  console.log('Клик по кнопке меню');
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom_visible');
+var menuButton = $('.menu-button');
+menuButton.on('click', function () {
+  $('.navbar-bottom').toggleClass('navbar-bottom_visible');
+});
+
+  var modalButton = $('[data-toggle=modal]');
+  var closeModalButton = $('.modal__close');
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+    
+  function openModal() {
+    var modalOwerLay = $('.modal__owerlay');
+    var modalDialog = $('.modal__dialog');
+    modalOwerLay.addClass('modal__owerlay_visible');
+    modalDialog.addClass('modal__dialog_visible');
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOwerLay = $('.modal__owerlay');
+    var modalDialog = $('.modal__dialog');
+    modalOwerLay.removeClass('modal__owerlay_visible');
+    modalDialog.removeClass('modal__dialog_visible');
+  }
 });
